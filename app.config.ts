f56593dae957b354d79a3ee7b6c34d79f051b487
rootEnginear <rootenginear@users.noreply.github.com>
2025-10-19 02:01:58 +0700

    fix: build baseUrl

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,9 @@
+/// <reference types="node" />
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   server: {
+    baseURL: process.env.BASE_PATH ?? "/",
     prerender: {
       routes: ["/"],
       crawlLinks: true,
