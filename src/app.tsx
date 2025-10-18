a6604d530845720eb195249c7c297ae722a5e795
rootEnginear <rootenginear@users.noreply.github.com>
2025-10-19 02:41:35 +0700

    fix: basepath router

--- src/app.tsx
+++ src/app.tsx
@@ -7,6 +7,7 @@ import "./app.css";
 export default function App() {
   return (
     <Router
+      base={import.meta.env.SERVER_BASE_URL}
       root={(props) => (
         <MetaProvider>
           <Title>Can I nest... HTML tag nesting checker</Title>
